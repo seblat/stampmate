1ba9a427585a2040647d9306f24256e6042cafea
Sebastian Latacz <sebastian@gitmate.io>
2018-11-22 16:57:22 +0100

    Add batch stamping

--- stampmate.py
+++ stampmate.py
@@ -1,10 +1,18 @@
 #! /usr/bin/python3
 
 """
+Stamp a set of files with their number taken from their file name.
+The numeration within the naming has to be continous.
+Eg.:
+25.pdf --> 25_num.pdf
+26.pdf --> 26_num.pdf
+27.pdf --> 27_num.pdf
 requirements:
 - PyMuPDF
 usage:
-python3 stampmate <number> <file_name>
+python3 stampmate <first_file_number> <folder>
+Eg.:
+python3 stampmate.py 19 ./test-files
 """
 
 import os
@@ -22,22 +30,31 @@ try:
     assert len(sys.argv) == 3  # filename and two args
     assert sys.argv[1].isdigit()  # first arg is number
     assert os.path.exists(sys.argv[2])  # file exists
-    assert sys.argv[2].lower().endswith('.pdf')
+
 except AssertionError:
     print('please pass the number and the file name')
     sys.exit(1)
 
 # add text and write to <filename>_num.pdf
-number = int(sys.argv[1])
-text = "Rechnung Nr. {}".format(number)
-file_name = sys.argv[2]
-new_file_name = file_name[:-4]+"_num"+file_name[-4:]
+file_number = int(sys.argv[1])
+folder = sys.argv[2]
 
-doc = fitz.open(file_name)
-page = doc[0]
-page.insertText(fitz.Point(60, 60),
-                text,
-                fontsize=30,
-                color=(255/255, 128/255, 0/255))
-doc.save(new_file_name)
-print("Document saved as", new_file_name)
+
+while os.path.exists(f'{folder}/{file_number}.pdf'):
+    text = "Rechnung Nr. {}".format(file_number)
+    new_file_name = f'{file_number}_num.pdf'
+    doc = fitz.open(f'{folder}/{file_number}.pdf')
+    page = doc[0]
+    page.insertText(fitz.Point(60, 60),
+                    text,
+                    fontsize=30,
+                    color=(255/255, 128/255, 0/255))
+    doc.save(f'{folder}/{new_file_name}')
+    print("Document saved as", new_file_name)
+    file_number += 1
+
+else:
+    if file_number == int(sys.argv[1]): #No file has been stamped
+        print(f'file_number {file_number} was not found in {folder}')
+    else:
+        print(f'All files in {folder} from {sys.argv[1]} upwards haven been stamped.')
